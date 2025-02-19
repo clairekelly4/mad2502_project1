5ba6f1c599b1affce62450738f546ce8d51fe19c
DNSL\danil <danilpz05@gmail.com>
2025-02-19 18:42:39 -0500

    Second revision of riemann.py that does not use iteration.

--- riemann.py
+++ riemann.py
@@ -1,18 +1,12 @@
 import numpy as np
 def left_endpoint(x_vals: np.ndarray, func: np.ufunc):
-    cumulative_sum = 0
-    for i in range(len(x_vals)-1):
-        cumulative_sum += func(x_vals[i]) * (x_vals[i + 1] - x_vals[i])
-    return cumulative_sum
+    dist = x_vals[:-1] - x_vals[1:]
+    return np.sum(dist * func(x_vals[-1]))
 
 def trapezoid (x_vals: np.ndarray, func: np.ufunc):
-    cumulative_sum = 0
-    for i in range(len(x_vals)-1):
-        cumulative_sum += (func(x_vals[i]) + func(x_vals[i + 1]))/2 * (x_vals[i + 1] - x_vals[i])
-    return cumulative_sum
+    dist = x_vals[:-1] - x_vals[1:]
+    return np.sum((func(x_vals[:-1]) + func(x_vals[1:]))/2 * dist)
 
 def simpson (x_vals: np.ndarray, func: np.ufunc):
-    cumulative_sum = 0
-    for i in range(len(x_vals)-1):
-        cumulative_sum += (1/6) * (func(x_vals[i]) + 4 * func((x_vals[i] + x_vals[i + 1])/2) + func(x_vals[i + 1])) * (x_vals[i + 1] - x_vals[i])
-    return cumulative_sum
+    dist = x_vals[:-1] - x_vals[1:]
+    return np.sum((1/6) * (func(x_vals[:-1]) + 4 * func((x_vals[:-1] + x_vals[1:])/2) + func(x_vals[1:])) * dist)
