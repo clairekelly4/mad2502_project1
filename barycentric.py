c5c9fcbaa10a42a9145c02269b5b3c8b09db2a72
bhavya-kam <b.kambara@ufl.edu>
2025-02-20 22:02:48 -0500

    Update barycentric.py

--- barycentric.py
+++ barycentric.py
@@ -3,7 +3,7 @@ import numpy as np
 def get_barycentric_coordinates(triangle_coordinates: np.ndarray, point_coordinates: np.ndarray) -> np.ndarray:
     x1, x2, x3 = triangle_coordinates[0] # sets the three numbers in first row of array to x1, x2, and x3 for calculations
     y1, y2, y3 = triangle_coordinates[1] # sets the three numbers in second row of array to y1, y2, and y3 for calculations
-    x, y = point_coordinates[0], point_coordinates[1] # sets the x point coordinate to x and y point coordinate to y
+    x, y = point_coordinates[0], point_coordinates[1] # assigns the x point coordinate to x and y point coordinate to y
     l1 = ((x2 - x) * (y3 - y) - (x3 - x) * (y2 - y)) / ((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) # calculates lambda 1
     l2 = ((x3 - x) * (y1 - y) - (x1 - x) * (y3 - y)) / ((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) # calculates lambda 2
     l3 = 1 - l1 - l2 # calculates lamba 3
